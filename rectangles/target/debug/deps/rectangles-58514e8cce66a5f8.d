ae94dc0ff7fe29b946970b4fcb0ef7d5b5092a03
LichiBruno <lichiii@pm.me>
2022-03-16 01:39:41 -0300

    more structs

---
+++ rectangles/target/debug/deps/rectangles-58514e8cce66a5f8.d
@@ -0,0 +1,5 @@
+/home/lichi/projects/rectangles/target/debug/deps/rectangles-58514e8cce66a5f8: src/main.rs
+
+/home/lichi/projects/rectangles/target/debug/deps/rectangles-58514e8cce66a5f8.d: src/main.rs
+
+src/main.rs:
---
+++ rectangles/target/debug/rectangles.d
@@ -0,0 +1,1 @@
+/home/lichi/projects/rectangles/target/debug/rectangles: /home/lichi/projects/rectangles/src/main.rs
